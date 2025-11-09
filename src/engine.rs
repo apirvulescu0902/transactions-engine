ee917d4cb95bc699a51363e2e924cf2dc51c427f
Alexandra Pirvulescu <alexandra@Alexandras-MacBook-Air.local>
2025-11-09 21:30:32 +0200

    Add unit tests for engine

--- src/engine.rs
+++ src/engine.rs
@@ -73,3 +73,118 @@ impl TransactionsEngine {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal::Decimal;
+
+    use crate::types::TransactionType;
+
+    use super::TransactionsEngine;
+
+    #[test]
+    fn test_process_transaction() {
+        let mut engine = TransactionsEngine::new();
+        let client_id = 1;
+
+        let deposit_tx = TransactionType::Deposit {
+            client: client_id,
+            tx: 1,
+            amount: Decimal::new(3, 0),
+        };
+        engine
+            .process_transaction(deposit_tx)
+            .expect("Could not process deposit.");
+
+        let withdrawal_tx = TransactionType::Withdrawal {
+            client: client_id,
+            tx: 2,
+            amount: Decimal::new(2, 0),
+        };
+        engine
+            .process_transaction(withdrawal_tx)
+            .expect("Could not process withdrawal.");
+
+        let client = engine.clients.get(&client_id).unwrap();
+        assert_eq!(client.available, Decimal::new(1, 0));
+        assert_eq!(client.total, Decimal::new(1, 0));
+        assert_eq!(client.held, Decimal::new(0, 0));
+        assert!(!client.locked);
+        assert!(client.processed_transactions.contains_key(&1));
+
+        let deposit_tx = TransactionType::Deposit {
+            client: client_id,
+            tx: 3,
+            amount: Decimal::new(3, 0),
+        };
+        engine
+            .process_transaction(deposit_tx.clone())
+            .expect("Could not process deposit.");
+
+        // check that processing the same transaction twice fails
+        assert!(engine.process_transaction(deposit_tx).is_err());
+
+        let resolve_tx = TransactionType::Resolve {
+            client: client_id,
+            tx: 3,
+        };
+
+        // check that resolving an undisputed transaction fails
+        assert!(engine.process_transaction(resolve_tx.clone()).is_err());
+
+        let dispute_tx = TransactionType::Dispute {
+            client: client_id,
+            tx: 3,
+        };
+        engine
+            .process_transaction(dispute_tx)
+            .expect("Could not dispute transaction.");
+
+        let client = engine.clients.get(&client_id).unwrap();
+        assert_eq!(client.available, Decimal::new(1, 0));
+        assert_eq!(client.total, Decimal::new(4, 0));
+        assert_eq!(client.held, Decimal::new(3, 0));
+        assert!(!client.locked);
+
+        engine
+            .process_transaction(resolve_tx)
+            .expect("Could not resolve transaction");
+
+        let client = engine.clients.get(&client_id).unwrap();
+        assert_eq!(client.available, Decimal::new(4, 0));
+        assert_eq!(client.total, Decimal::new(4, 0));
+        assert_eq!(client.held, Decimal::new(0, 0));
+        assert!(!client.locked);
+
+        let deposit_tx = TransactionType::Deposit {
+            client: client_id,
+            tx: 4,
+            amount: Decimal::new(1, 0),
+        };
+        engine
+            .process_transaction(deposit_tx)
+            .expect("Could not process deposit.");
+
+        let dispute_tx = TransactionType::Dispute {
+            client: client_id,
+            tx: 4,
+        };
+        engine
+            .process_transaction(dispute_tx)
+            .expect("Could not dispute transaction.");
+
+        let chargeback_tx = TransactionType::Chargeback {
+            client: client_id,
+            tx: 4,
+        };
+        engine
+            .process_transaction(chargeback_tx)
+            .expect("Could not chargeback transaction.");
+
+        let client = engine.clients.get(&client_id).unwrap();
+        assert_eq!(client.available, Decimal::new(4, 0));
+        assert_eq!(client.total, Decimal::new(4, 0));
+        assert_eq!(client.held, Decimal::new(0, 0));
+        assert!(client.locked);
+    }
+}
